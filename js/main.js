7031b7b397261b7514b8a650a7f00855aa8b0f66
Maaike Seijsener <36770@ma-web.nl>
2023-10-16 12:41:11 +0200

    Haikup

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ oldgreeting.greet()
 
 function basicHaiku()
 {
-    return ["Toward those short trees","We saw a batman descending","They fly up so high."]
+    return ["Toward those short trees","Something struck me in the rear","They fly up so high."]
 }
 //zet hier je haiku functie neer, zie https://github.com/progsen/haikugitopdracht voor ideeen
 
